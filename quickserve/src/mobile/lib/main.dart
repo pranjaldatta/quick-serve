7aefacb36f43757c285a012e6f97bd6b26d44bf6
Aniket Sinha <aniketkishore100@gmail.com>
2020-09-19 14:52:21 +0530

    Basic UI and integration with the ML model done offline

--- quickserve/src/mobile/lib/main.dart
+++ quickserve/src/mobile/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 void main() => runApp(MaterialApp(
       routes: {
@@ -49,7 +52,7 @@ class _MyAppState extends State<MyApp> {
             ),
             SizedBox(height: 40),
             new RaisedButton(
-              onPressed: () => {},
+              onPressed: () => Navigator.pushNamed(context, 'upload'),
               color: Color.fromRGBO(254, 211, 44, 1),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(50.0),
@@ -73,18 +76,71 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromRGBO(32, 36, 42, 1),
-        body: Container(
-            child: Column(
+        body: _loading
+            ? Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _image == null
+                        ? Container(
+                            child: Text(
+                              'Choose or Capture an Image',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color.fromRGBO(254, 211, 44, 1),
+                                  fontSize: 50.0,
+                                  fontFamily: "Roboto"),
+                            ),
+                          )
+                        : Image.file(_image),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    _outputs != null
+                        ? Text(
+                            "Prediction: ${_outputs[0]["label"]}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          )
+                        : Container()
+                  ],
+                ),
+              ),
+        floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
               heroTag: null,
               backgroundColor: Color.fromRGBO(254, 211, 44, 1),
-              onPressed: () {},
+              onPressed: pickImageCam,
               child: Icon(
                 Icons.photo_camera,
                 color: Colors.black,
@@ -94,13 +150,60 @@ class _UploadState extends State<Upload> {
             FloatingActionButton(
               heroTag: null,
               backgroundColor: Color.fromRGBO(254, 211, 44, 1),
-              onPressed: () {},
+              onPressed: pickImage,
               child: Icon(
                 Icons.image,
                 color: Colors.black,
               ),
             ),
           ],
-        )));
+        ));
+  }
+
+  pickImageCam() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.2,
+      imageMean: 0,
+      imageStd: 255.0,
+    );
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
   }
 }
